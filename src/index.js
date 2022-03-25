5c1598ddb5236c67a7ec5f1bfbb6d410ccd2bf63
neyla sanjuan <neyla.sanjuan@archidep.com>
2022-03-25 11:54:12 +0100

    changements 2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import * as d3 from 'd3';
 
 
 //import file1 from "../datasets/chartsSwissPlus2021.csv"
-import file2 from "../datasets/data_virgule.csv"
+import file2 from "../datasets/data.csv"
 
 console.log(file2)
 
