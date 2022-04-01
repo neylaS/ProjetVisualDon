ec40812b472000164f8ace48c19c5a2e94f7bae0
neyla sanjuan <neyla.sanjuan@archidep.com>
2022-04-01 09:49:50 +0200

    ajout données carte

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import * as d3 from 'd3';
 
 
 //import file1 from "../datasets/chartsSwissPlus2021.csv"
-import file2 from "../datasets/data.csv"
+//import file2 from "../datasets/data.csv"
 
-console.log(file2)
+// console.log(file2)
 
 
-const artist = file2.map((d, i) => {
-				return d.Artist;
-			    	});
-console.log(artist)
+// const artist = file2.map((d, i) => {
+// 				return d.Artist;
+// 			    	});
+// console.log(artist)
+
+const width = 550, height = 550;
+const path = d3.geoPath();
+const projection = d3.geoConicConformal()
+    .center([2.454071, 46.279229])
+    .scale(2600)
+    .translate([width / 2, height / 2]);
