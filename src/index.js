5dadd12a7109f0e34aa5408e226793fec40b6c3a
neyla sanjuan <neyla.sanjuan@archidep.com>
2022-05-05 17:20:06 +0200

    clean data

--- src/index.js
+++ src/index.js
@@ -6,37 +6,7 @@ import data from '../data/projet_dataviz.geojson'
 
 import './pays.js'
 
-const margin = {
-        top: 60,
-        right: 45,
-        bottom: 60,
-        left: 45
-    },
-    width = window.innerWidth - margin.left - margin.right,
-    height = window.innerHeight - margin.top - margin.bottom;
 
-const svg = d3.select("#map")
-    .append("svg")
-    .attr("width", width + margin.right)
-    .attr("height", height + margin.bottom)
-    // translate this svg element to leave some margin.
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
-
-let projection = d3.geoMercator()
-    .fitSize([width, height], data)
-
-let path = d3.geoPath()
-    .projection(projection)
-
-svg.selectAll("path")
-    .data(data.features)
-    .join(enter => enter.append('path')
-        .attr("d", path)
-        .attr("fill", d => d.properties.NAME == 'Switzerland' ? '#1DB954' : 'transparent')
-        .attr("stroke", 'black')
-        .attr("stroke-width", 2)
-    )
 
 d3.select("head title")
     .text(d.properties.NAME)
