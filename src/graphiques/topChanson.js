b5c97bda9c2d2f130393ddceeeef9a80d5853906
neyla sanjuan <neyla.sanjuan@archidep.com>
2022-05-10 09:12:35 +0200

    graph avec photos

--- src/graphiques/topChanson.js
+++ src/graphiques/topChanson.js
@@ -1,6 +1,7 @@
-
 import * as d3 from 'd3';
 import data from '../../data/data-20.csv';
+import {event as currentEvent} from 'd3-selection';
+
 
 // show the 5 first tracks at the position 0,1 and 2 for the date : 01.01.21 
 let tracksByDay = data.filter(d => d.Date === "01.01.21");
@@ -48,188 +49,327 @@ console.log(tracksByDay4[4])
 
 var data1 = [
 
-    {group: "1ère Position", value: 5065542, src: "assets/img/Badbunny.jpg"},
-    {group: "2ème Position", value: 3764813, src: "assets/img/c"},
-    {group: "3ème Position", value: 3560869, src: "assets/img/Karolg.jpg"},
-    {group: "4ème Position", value: 3402064, src: "assets/img/24Goldn.jpg"},
-    {group: "5ème Position", value: 2939820, src: "assets/img/Arianagrande.jpg"}
- ];
- 
- var data2 = [
-    {group: "1ère Position", value: 7647230,src: "assets/img/Oliviarodrigo.jpg"},
-    {group: "2ème Position", value: 3735645,src: "assets/img/Badbunny.jpg"},
-    {group: "3ème Position", value: 3013315,src: "assets/img/Theweeknd.jpg"},
-    {group: "4ème Position", value: 3000536,src: "assets/img/Theweeknd.jpg"},
-    {group: "5ème Position", value: 2858425,src: "assets/img/24Goldn.jpg"}
- ];
+    {
+        group: "1ère Position",
+        value: 5065542,
+        src: "assets/img/Badbunny.png",
+        legend : "Dakiti de Bad Bunny"
+    },
+    {
+        group: "2ème Position",
+        value: 3764813,
+        src: "assets/img/Theweeknd.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "3ème Position",
+        value: 3560869,
+        src: "assets/img/Karolg.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "4ème Position",
+        value: 3402064,
+        src: "assets/img/24Goldn.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "5ème Position",
+        value: 2939820,
+        src: "assets/img/Ariannagrande.png",
+        legend : "Blinding Lights de The Weeknd"
+    }
+];
 
- var data3 = [
-    {group: "1ère Position", value: 5202794, src: "assets/img/Oliviarodrigo.jpg" },
-    {group: "2ème Position", value: 3966280, src: "assets/img/Theweeknd.jpg"},
-    {group: "3ème Position", value: 3280968, src: "assets/img/Theweeknd.jpg"},
-    {group: "4ème Position", value: 3156979, src: "assets/img/Kaliuchis.jpg"},
-    {group: "5ème Position", value: 3151274, src: "assets/img/Badbunny.jpg"}
- ];
- var data4 = [
-    {group: "1ère Position", value: 7540770,src: "assets/img/Justinbieber.jpg"},
-    {group: "2ème Position", value: 6980543,src: "assets/img/Lilnasx.jpg"},
-    {group: "3ème Position", value: 4426518,    src: "assets/img/Maskwolf.jpg"},
-    {group: "4ème Position", value: 3905019,    src: "assets/img/Oliviarodrigo.jpg"},
-    {group: "5ème Position", value: 3753723,    src: "assets/img/Theweeknd.jpg"}
- ];
+var data2 = [{
+        group: "1ère Position",
+        value: 7647230,
+        src: "assets/img/Oliviarodrigo.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "2ème Position",
+        value: 3735645,
+        src: "assets/img/Badbunny.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "3ème Position",
+        value: 3013315,
+        src: "assets/img/Theweeknd.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "4ème Position",
+        value: 3000536,
+        src: "assets/img/Theweeknd.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "5ème Position",
+        value: 2858425,
+        src: "assets/img/24Goldn.png",
+        legend : "Blinding Lights de The Weeknd"
+    }
+];
 
- 
- // set the dimensions and margins of the graph
- var margin = {top: 30, right: 30, bottom: 70, left: 60},
-     width = 600 - margin.left - margin.right,
-     height = 600 - margin.top - margin.bottom;
- 
+var data3 = [{
+        group: "1ère Position",
+        value: 5202794,
+        src: "assets/img/Oliviarodrigo.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "2ème Position",
+        value: 3966280,
+        src: "assets/img/Theweeknd.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "3ème Position",
+        value: 3280968,
+        src: "assets/img/Theweeknd.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "4ème Position",
+        value: 3156979,
+        src: "assets/img/Kaliuchis.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "5ème Position",
+        value: 3151274,
+        src: "assets/img/Badbunny.png",
+        legend : "Blinding Lights de The Weeknd"
+    }
+];
+var data4 = [{
+        group: "1ère Position",
+        value: 7540770,
+        src: "assets/img/Justinbieber.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "2ème Position",
+        value: 6980543,
+        src: "assets/img/Lilnasx.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "3ème Position",
+        value: 4426518,
+        src: "assets/img/Maskwolf.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "4ème Position",
+        value: 3905019,
+        src: "assets/img/Oliviarodrigo.png",
+        legend : "Blinding Lights de The Weeknd"
+    },
+    {
+        group: "5ème Position",
+        value: 3753723,
+        src: "assets/img/Theweeknd.png",
+        legend : "Blinding Lights de The Weeknd"
+    }
+];
 
- // append the svg object to the body of the page
+
+
+// set the dimensions and margins of the graph
+var margin = {
+        top: 30,
+        right: 30,
+        bottom: 70,
+        left: 60
+    },
+    width = 600 - margin.left - margin.right,
+    height = 600 - margin.top - margin.bottom;
+
+
+// append the svg object to the body of the page
 let svg2 = d3.select("#topChanson")
-   .append("svg")
-     .attr("width", width + margin.left + margin.right)
-     .attr("height", height + margin.top + margin.bottom)
-   .append("g")
-     .attr("transform",
-           "translate(" + margin.left + "," + margin.top + ")");
- 
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
- var x = d3.scaleBand()
-   .range([ 0, width ])
-   .domain(data1.map(function(d) { return d.group; }))
-   .padding(0.2);
- svg2.append("g")
-   .attr("transform", "translate(0," + height + ")")
-   .call(d3.axisBottom(x))
- 
- // Add Y axis
- var y = d3.scaleLinear()
-   .domain([0, 8000000])
-   .range([ height, 0]);
- svg2.append("g")
-   .attr("class", "myYaxis")
-   .call(d3.axisLeft(y));
- 
 
- // A function that create / update the plot for a given variable:
- function update(data) {
- 
-   var u = svg2.selectAll("rect")
-     .data(data)
- 
-   u
-     .enter()
-     .append("rect")
-     .merge(u)
-     .transition()
-     .duration(1000)
-       .attr("x", function(d) { return x(d.group); })
-       .attr("y", function(d) { return y(d.value); })
-       .attr("width", x.bandwidth())
-       .attr("height", function(d) { return height - y(d.value); })
-       .attr("fill", "#69b3a2")
-       .attr("opacity", 0.5)
-    //add a circle to the top of the bars to put an image
-    svg2.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    //puut the circles on the top of the bars in 
-    .attr("cx", function(d) { return x(d.group) + x.bandwidth() / 2; })
-    .attr("cy", function(d) { return y(d.value); })
-    .attr("r", 20)
-    .attr("fill", "#69b3a2")
-    .attr("stroke", "#69b3a2")
-    .attr("stroke-width", 1)
-    .attr("opacity", 1)
+var x = d3.scaleBand()
+    .range([0, width])
+    .domain(data1.map(function (d) {
+        return d.group;
+    }))
+    .padding(0.2);
+svg2.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+
+// Add Y axis
+var y = d3.scaleLinear()
+    .domain([0, 8000000])
+    .range([height, 0]);
+svg2.append("g")
+    .attr("class", "myYaxis")
+    .call(d3.axisLeft(y));
+
+
+
+
+
+
+// A function that create / update the plot for a given variable:
+function update(data) {
+
+    var u = svg2.selectAll("rect")
+        .data(data)
+
+    u.enter()
+        .append("rect")
+        .merge(u)
+        .transition()
+        .duration(1000)
+        .attr("x", function (d) {
+            return x(d.group);
+        })
+        .attr("y", function (d) {
+            return y(d.value);
+        })
+        .attr("width", x.bandwidth())
+        .attr("height", function (d) {
+            return height - y(d.value);
+        })
+        .attr("fill", "#69b3a2")
+        .attr("opacity", 0.5)
+
+    
     //append an image to the circles
     svg2.selectAll("image")
-    .data(data)
-    .enter()
-    .append("image")
-    .attr("xlink:href", function(d) { return d.src; })
-    .attr("x", function(d) { return x(d.group) + x.bandwidth() / 2 - 20; })
-    .attr("y", function(d) { return y(d.value) - 20; })
-    .attr("width", 40)
-    .attr("height", 40)
-    .attr("opacity", 1)
-
-  
-   
-
-        
-
- }
- 
- // Initialize the plot with the first dataset
- update(data1)
- //when on click on the button, update the plot with the second dataset and the position of the circles
+        .data(data)
+        .enter()
+        .append("image")
+        .attr("xlink:href", function (d) {
+            return d.src;
+        })
+        .attr("x", function (d) {
+            return x(d.group) + x.bandwidth() / 2 - 20;
+        })
+        .attr("y", function (d) {
+            return y(d.value) - 20;
+        })
+        .attr("width", 40)
+        .attr("height", 40)
+        .attr("opacity", 1)
 
 
-    d3.select("#button2").on("click", function() {
-        update(data2)
-        d3.selectAll("circle").remove()
-        svg2.selectAll("circle")
+
+}
+
+// Initialize the plot with the first dataset
+update(data1)
+//when on click on the button, update the plot with the second dataset and the position of the circles
+
+//when on click on the button, update the images with the second dataset and the position of the circles
+
+
+
+
+d3.select("#button2").on("click", function () {
+    update(data2)
+    d3.selectAll("image").remove()
+    svg2.selectAll("image")
         .data(data2)
         .enter()
-        .append("circle")
-        .attr("cx", function(d) { return x(d.group) + x.bandwidth() / 2; })
-        .attr("cy", function(d) { return y(d.value); })
-        .attr("r", 20)
-        .attr("fill", "#69b3a2")
-        .attr("stroke", "#69b3a2")
-        .attr("stroke-width", 1)
+        .append("image")
+        .attr("xlink:href", function (d) {
+            return d.src;
+        })
+        .attr("x", function (d) {
+            return x(d.group) + x.bandwidth() / 2 - 20;
+        })
+        .attr("y", function (d) {
+            return y(d.value) - 20;
+        })
+        .attr("width", 40)
+        .attr("height", 40)
         .attr("opacity", 1)
-    })
-    
-    //when on click on the button, update the plot with the third dataset
-    d3.select("#button3").on("click", function() {
-        update(data3)
-        d3.selectAll("circle").remove()
-        svg2.selectAll("circle")
+
+})
+
+
+
+
+
+
+//when on click on the button, update the plot with the third dataset
+d3.select("#button3").on("click", function () {
+    update(data3)
+    d3.selectAll("image").remove()
+    svg2.selectAll("image")
         .data(data3)
         .enter()
-        .append("circle")
-        .attr("cx", function(d) { return x(d.group) + x.bandwidth() / 2; })
-        .attr("cy", function(d) { return y(d.value); })
-        .attr("r", 20)
-        .attr("fill", "#69b3a2")
-        .attr("stroke", "#69b3a2")
-        .attr("stroke-width", 1)
+        .append("image")
+        .attr("xlink:href", function (d) {
+            return d.src;
+        })
+        .attr("x", function (d) {
+            return x(d.group) + x.bandwidth() / 2 - 20;
+        })
+        .attr("y", function (d) {
+            return y(d.value) - 20;
+        })
+        .attr("width", 40)
+        .attr("height", 40)
         .attr("opacity", 1)
-    })
-    
-    //when on click on the button, update the plot with the fourth dataset
-    d3.select("#button4").on("click", function() {
-        update(data4)
-        d3.selectAll("circle").remove()
-        svg2.selectAll("circle")
+
+})
+
+//when on click on the button, update the plot with the fourth dataset
+d3.select("#button4").on("click", function () {
+    update(data4)
+    d3.selectAll("image").remove()
+    svg2.selectAll("image")
         .data(data4)
         .enter()
-        .append("circle")
-        .attr("cx", function(d) { return x(d.group) + x.bandwidth() / 2; })
-        .attr("cy", function(d) { return y(d.value); })
-        .attr("r", 20)
-        .attr("fill", "#69b3a2")
-        .attr("stroke", "#69b3a2")
-        .attr("stroke-width", 1)
+        .append("image")
+        .attr("xlink:href", function (d) {
+            return d.src;
+        })
+        .attr("x", function (d) {
+            return x(d.group) + x.bandwidth() / 2 - 20;
+        })
+        .attr("y", function (d) {
+            return y(d.value) - 20;
+        })
+        .attr("width", 40)
+        .attr("height", 40)
         .attr("opacity", 1)
-    })
-    //when on click on the button, update the plot with the first dataset
-    d3.select("#button1").on("click", function() {
-        update(data1)
-        d3.selectAll("circle").remove()
-        svg2.selectAll("circle")
+
+})
+//when on click on the button, update the plot with the first dataset
+d3.select("#button1").on("click", function () {
+    update(data1)
+    d3.selectAll("image").remove()
+    svg2.selectAll("image")
         .data(data1)
         .enter()
-        .append("circle")
-        .attr("cx", function(d) { return x(d.group) + x.bandwidth() / 2; })
-        .attr("cy", function(d) { return y(d.value); })
-        .attr("r", 20)
-        .attr("fill", "#69b3a2")
-        .attr("stroke", "#69b3a2")
-        .attr("stroke-width", 1)
+        .append("image")
+        .attr("xlink:href", function (d) {
+            return d.src;
+        })
+        .attr("x", function (d) {
+            return x(d.group) + x.bandwidth() / 2 - 20;
+        })
+        .attr("y", function (d) {
+            return y(d.value) - 20;
+        })
+        .attr("width", 40)
+        .attr("height", 40)
         .attr("opacity", 1)
-    })
-    
+
+})
+
