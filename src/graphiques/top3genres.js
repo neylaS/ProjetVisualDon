9a9d64f121b252d8b69a5498a24ac25cfb1183fa
neyla sanjuan <neyla.sanjuan@archidep.com>
2022-05-13 00:09:42 +0200

    merge

--- src/graphiques/top3genres.js
+++ src/graphiques/top3genres.js
@@ -3,9 +3,105 @@ import * as d3 from 'd3';
 /* import data from '../../data/data-20.csv'; */
 import data from '../../data/top3genres.json';
 
+/// set the dimensions and margins of the graph
+const width = 500,
+height = 500,
+margin = 90;
 
-  
-      
+// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
+const radius = Math.min(width, height) / 2 - margin
+
+// append the svg object to the div called 'my_dataviz'
+const svg = d3.select("#top3genres .content")
+.append("svg")
+.attr("width", width)
+.attr("height", height)
+.append("g")
+.attr("transform", `translate(${width/2},${height/2})`);
+
+// Create dummy data
+data = {TrapLatino: 38, Latin: 38, Reggaeton:38, PopRap:38, CanadianComtemporaryRandB:38, PostTeenPop:38, Pop:38, AustralianHipHop :38, DancePop: 38, CanadianPop: 38}
+
+/* // set the color scale
+const color = d3.scaleOrdinal()
+.domain(["TrapLatino", "Latin",  "Reggaeton" , "PopRap" , "CanadianComtemporaryRandB " , "PostTeenPop", "Pop" , "AustralianHipHop", "DancePop",  "CanadianPop"])
+.range(d3.schemeDark2); */
+
+// Compute the position of each group on the pie:
+const pie = d3.pie()
+.sort(null) // Do not sort group by size
+.value(d => d[1])
+const data_ready = pie(Object.entries(data))
+
+// The arc generator
+const arc = d3.arc()
+.innerRadius(radius * 0.5)         // This is the size of the donut hole
+.outerRadius(radius * 0.8)
+
+// Another arc that won't be drawn. Just for labels positioning
+const outerArc = d3.arc()
+.innerRadius(radius * 0.9)
+.outerRadius(radius * 0.9)
+
+// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
+svg
+.selectAll('allSlices')
+.data(data_ready)
+.join('path')
+.attr('d', arc)
+.attr('fill', "red")
+.attr("stroke", "white")
+.style("stroke-width", "2px")
+.style("opacity", 0.7)
+
+// Add the polylines between chart and labels:
+svg
+.selectAll('allPolylines')
+.data(data_ready)
+.join('polyline')
+.attr("stroke", "black")
+.style("fill", "none")
+.attr("stroke-width", 1)
+.attr('points', function(d) {
+  const posA = arc.centroid(d) // line insertion in the slice
+  const posB = outerArc.centroid(d) // line break: we use the other arc generator that has been built only for that
+  const posC = outerArc.centroid(d); // Label position = almost the same as posB
+  const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2 // we need the angle to see if the X position will be at the extreme right or extreme left
+  posC[0] = radius * 0.95 * (midangle < Math.PI ? 1 : -1); // multiply by 1 or -1 to put it on the right or on the left
+  return [posA, posB, posC]
+})
+
+// Add the polylines between chart and labels:
+svg
+.selectAll('allLabels')
+.data(data_ready)
+.join('text')
+.text(d => d.data[0])
+.attr('transform', function(d) {
+    const pos = outerArc.centroid(d);
+    const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+    pos[0] = radius * 0.99 * (midangle < Math.PI ? 1 : -1);
+    return `translate(${pos})`;
+})
+.style('text-anchor', function(d) {
+    const midangle = d.startAngle + (d.endAngle - d.startAngle) / 2
+    return (midangle < Math.PI ? 'start' : 'end')
+})
+    .on("click", function(event) {
+      var nomArtist = this.getAttribute("data-name");
+      var legendArtist = this.getAttribute("data-legend");
+      var srcImg = this.getAttribute("href");
+
+      document.querySelector('.namePopup').innerText = nomArtist;
+      document.querySelector('.legendPopup').innerText = legendArtist;
+      document.querySelector('.imgPopup').src = srcImg;
+      document.querySelector('.lienSpotify').href = "https://open.spotify.com/search/" + legendArtist
+
+
+      document.getElementById("abc").style.display = "block";
+    })
+
+
 
  /* //get the list of 10 most recurent genre in data set
  let genres = data.map(d => d.Genre.replace("[", "").replace("]", "").replaceAll("'","").trim().split(","));
@@ -431,6 +527,4 @@ let PopRap = {
 // A scale that gives a X target position for each group
 
 //create the svg element
-    
- 
  
